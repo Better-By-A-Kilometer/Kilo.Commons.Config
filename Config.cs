5c2bee8bec15c315fc8d2aa9435702398ed48c72
Kilo <37485242+DevKilo1@users.noreply.github.com>
2025-02-28 13:58:11 -0500

    Init Config

--- Config.cs
+++ Config.cs
@@ -1,6 +1,27 @@
-﻿namespace Kilo.Commons.Config
+﻿using System.Collections.Generic;
+using CitizenFX.Core.Native;
+using Newtonsoft.Json.Linq;
+
+namespace Kilo.Commons.Config
 {
-  public class Config
-  {
-  }
+    public class Config<T> where T : JContainer
+    {
+        public JContainer Store { get; }
+
+        public Config(string resourceName, string path)
+        {
+            path = path.Replace(resourceName, "");
+            Store = LoadResourceFile(resourceName, path);
+        }
+
+        public static T LoadResourceFile(string resourceName, string path)
+        {
+            var file = API.LoadResourceFile(resourceName, path);
+            if (file.StartsWith("[") && file.EndsWith("]"))
+            {
+                return JArray.Parse(file).ToObject<T>();
+            }
+            return JObject.Parse(file).ToObject<T>();
+        }
+    }
 }
